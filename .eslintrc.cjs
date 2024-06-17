13a214d3bafa46be53454e2141aa267e299128c3
Maxime Dupont <m.dupont103@gmail.com>
2024-06-17 10:11:07 +0200

    project config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,39 @@ module.exports = {
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
 			}
+		},
+		{
+			files: ['*.svelte', '*.ts'],
+			rules: {
+				// https://typescript-eslint.io/troubleshooting/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+				// typescript-eslint recommend that you do not use the no-undef lint rule on TypeScript projects.
+				// The checks it provides are already provided by TypeScript without the need for configuration
+				// TypeScript just does this significantly better.
+				'no-undef': 'off'
+			}
 		}
-	]
+	],
+	rules: {
+		// https://stackoverflow.com/a/64067915/11320442
+		// allows unused variable if they start with _
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_'
+			}
+		],
+		// https://eslint.org/docs/latest/rules/no-console
+		// disallows the use of console.info() and console.log()
+		'no-console': [
+			'error',
+			{
+				allow: ['warn', 'error']
+			}
+		],
+		// https://eslint.org/docs/latest/rules/no-control-regex
+		// allows to write new RegExp("\t") which is normally only possible with /\t/
+		'no-control-regex': 0
+	}
 };
