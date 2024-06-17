dc472b002eb09e248bcdf1844d36c182267c0561
Maxime Dupont <m.dupont103@gmail.com>
2024-06-17 11:05:52 +0200

    initialize green-washing-tech-companies talk

---
+++ src/routes/2024/green-washing-tech-companies/[...slug]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import {
+		RevealJsContext,
+		SvelteRevealHMR,
+		black,
+		monokai,
+		highlight,
+		markdown,
+		notes
+	} from 'svelte-reveal.js';
+	import * as Slide from './slides';
+</script>
+
+<RevealJsContext
+	options={{
+		controls: true,
+		progress: true,
+		center: true,
+		hash: true
+	}}
+	plugins={[highlight, markdown, notes]}
+	themes={[black, monokai]}
+>
+	<!-- use this only in dev -->
+	<!-- if you forget it, it's not a big deal though -->
+	<SvelteRevealHMR />
+
+	<Slide.Title />
+</RevealJsContext>
---
+++ src/routes/2024/green-washing-tech-companies/[...slug]/slides/index.ts
@@ -0,0 +1,38 @@
+export { default as Title } from './00-title.svelte';
+
+/**
+ * /000
+ * 
+ * l'empire
+ * moi
+ * 
+ * /100
+ * 
+ * introduction
+ * 
+ * /110
+ * 
+ * pledge
+ * 
+ * /120
+ * 
+ * products
+ * 
+ * /200
+ * 
+ * carbon + title
+ * 
+ * /210
+ * 
+ * scope
+ * 
+ * /220
+ * 
+ * carbon
+ * 
+ * /300
+ * 
+ * reveal
+ * remerciement
+ * 
+ */
